5b7312c3606dfc58648260bc9563f37fd73ce7af
Demut91 <psycho.cube@yandex.ru>
2022-04-17 00:53:26 +0600

    responsive added

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,15 +1,33 @@
 import styled from "styled-components";
 import { useState } from "react";
 import ApercuArabicPro from "./fonts/ApercuArabicPro-Regular.woff";
+import ld from "./img/icons/ld.svg";
+import tw from "./img/icons/tw.svg";
+import fb from "./img/icons/fb.svg";
+import pin from "./img/icons/pin.svg";
 
-const API = "https://ptsv2.com/t/labjq-1649938066/post";
+const API = "http://82.202.168.156:3000";
 
 const FormWrapper = styled.section`
   @font-face {
     font-family: "ApercuArabicPro";
     src: url(${ApercuArabicPro}) format("woff");
   }
-  padding: 150px 150px 174px 150px; ;
+  padding: 148px;
+  @media (max-width: 1200px) {
+    padding: 100px 100px 100px 100px;
+    text-align: center;
+  }
+  @media (max-width: 760px) {
+    padding: 80px;
+  }
+  @media (max-width: 500px) {
+    padding: 80px 15px;
+  }
+  @media (max-width: 400px) {
+    
+    padding: 80px 5px;
+  }
 `;
 
 const Title = styled.h1`
@@ -35,6 +53,17 @@ const Input = styled.input`
     font-weight: 500;
     letter-spacing: 0.4px;
   }
+  @media (max-width: 760px) {
+    min-width: 450px;
+  }
+  @media (max-width: 600px) {
+    min-width: 370px;
+    padding: 18px 20px;
+  }
+  @media (max-width: 400px) {
+    min-width: 318px;
+    padding: 10px 12px;
+  }
 `;
 const Textarea = styled(Input)``;
 
@@ -54,13 +83,26 @@ const Button = styled.button`
   }
 `;
 
+const SocialIcons = styled.div`
+  display: flex;
+`;
+
+const SocialLink = styled.button`
+  cursor: pointer;
+  margin: 13px;
+  border: none;
+  height: 18px;
+  width: 18px;
+  background: url(${(props) => props.img}) 100% 100% no-repeat;
+`;
+
 function Form() {
-  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [text, setText] = useState("");
 
-  const handleNameChange = (e) => {
-    setName(e.target.value);
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
   };
 
   const handleEmailChange = (e) => {
@@ -75,66 +117,78 @@ function Form() {
     e.preventDefault();
 
     await fetch(API, {
+      headers: {
+        "Content-Type": "application/json",
+      },
       method: "POST",
       body: JSON.stringify({
-        name: name,
+        name: username,
         email: email,
         text: text,
       }),
     })
-      .then(() => {
-        console.log("Отправлено");
+      .then((res) => {
+        console.log("Отправлено", res.status);
       })
       .catch(() => {
         alert("Sending error!");
       })
       .finally(() => {
-        setName("");
+        setUsername("");
         setEmail("");
         setText("");
       });
   }
 
   return (
-    <FormWrapper>
-      <Title>Reach out to us!</Title>
+    <>
+      <FormWrapper>
+        <Title>Reach out to us!</Title>
+        <form onSubmit={sendingData}>
+          <div>
+            <Input
+              required
+              type="text"
+              placeholder="Your name*"
+              onChange={handleUsernameChange}
+              value={username}
+            />
+          </div>
 
-      <form onSubmit={sendingData}>
-        <div>
-          <Input
-            required
-            type="text"
-            placeholder="Your name*"
-            onChange={handleNameChange}
-            value={name}
-          />
-        </div>
+          <div>
+            <Input
+              required
+              type="email"
+              placeholder="Your e-mail*"
+              onChange={handleEmailChange}
+              value={email}
+            />
+          </div>
 
-        <div>
-          <Input
-            required
-            type="email"
-            placeholder="Your e-mail*"
-            onChange={handleEmailChange}
-            value={email}
-          />
-        </div>
+          <div>
+            <Textarea
+              as="textarea"
+              required
+              type="text"
+              placeholder="Your message*"
+              onChange={handleTextChange}
+              value={text}
+              height="189px"
+            />
+          </div>
 
-        <div>
-          <Textarea
-            as="textarea"
-            required
-            type="text"
-            placeholder="Your message*"
-            onChange={handleTextChange}
-            value={text}
-            height="189px"
-          />
-        </div>
-
-        <Button>Send message</Button>
-      </form>
-    </FormWrapper>
+          <Button>Send message</Button>
+        </form>
+      </FormWrapper>
+      <footer>
+        <SocialIcons>
+          <SocialLink img={ld}></SocialLink>
+          <SocialLink img={tw}></SocialLink>
+          <SocialLink img={fb}></SocialLink>
+          <SocialLink img={pin}></SocialLink>
+        </SocialIcons>
+      </footer>
+    </>
   );
 }
 
